dc5f0137695798608ef6f2579b1ffe24b1c7df9e
David19 <davidleonel911@gmail.com>
2025-06-29 20:46:13 -0500

    nuevos ajustes

--- script.js
+++
@@ -1,7 +0,0 @@
-const botonRegistro = document.getElementById("boton-registro");
-
-botonRegistro.addEventListener("click", hacerClick)
-
-function hacerClick() {
-    alert("Has hecho click en el botón de registro");
-};
