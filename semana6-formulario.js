c793cb000972db673bff838b7c0b85fe7ae4f916
David19 <davidleonel911@gmail.com>
2025-07-06 10:48:13 -0500

    formulario con validaciones terminado

--- semana6-formulario.js
+++ semana6-formulario.js
@@ -9,59 +9,103 @@ Confirmación de contraseña (debe coincidir con la contraseña).
 Edad (debe ser mayor o igual a 18 años).
 */
 
-
-
 document.addEventListener("DOMContentLoaded", () => {//esperar a que cargue el DOM 
-
-
+ 
   let form = document.getElementById("form");
   let inputNombre = document.getElementById("inputNombre");
-  let parrafowarnings = document.getElementById("warnings");
+  let inputCorreo = document.getElementById("exampleInputEmail1");
+  let inputContra = document.getElementById("exampleInputPassword")
+  let inputContraRep = document.getElementById("exampleInputPassword2")
+  let inputEdad = document.getElementById("edad")
+  let confirmacionEntrada = document.getElementById("confirmar")
 
-  form.addEventListener("submit", e => {
-    e.preventDefault();
-    let warnings = "";
-    let entrar = false;
+  form.addEventListener("input", e => {
+    e.preventDefault();//evita que se recargue la pagina 
+     let entrar = true;//para que se vuelva a poner a true en caso de ser necesario 
+      //limpiar los erroes 
+      document.getElementById("errorNombre").innerHTML = "";
+      document.getElementById("errorCorreo").innerHTML = "";
+      document.getElementById("errorContra").innerHTML = "";
+      document.getElementById("errorContra1").innerHTML = "";
+      document.getElementById("errorContraRep").innerHTML = "";
+      document.getElementById("errorEdad").innerHTML = "";
+ 
 
-    if (inputNombre.value.length <= 6) {
-      warnings += `Nombre muy corto <br>`;
-      console.log("muy corto");
-      entrar = true;
+
+   
+    let emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; //expresion regular para validar correos usar ente /regex/
+    let contraRegExp = /[!@#$%^&*(),.?":{}|<>]/;
+    
+ 
+
+    if (inputNombre.value.length < 3) {//para el NOMBRE 
+    
+      document.getElementById("errorNombre").innerHTML = "El nombre es demasiado corto"
+      entrar = false
+      document.getElementById("inputNombre").style.border = "3px solid red"
+
+    }else{
+      document.getElementById("inputNombre").style.border = "none"
+
+    }
+    
+    if(!emailRegExp.test(inputCorreo.value)){//para el correo
+        document.getElementById("errorCorreo").innerHTML = "El correo no es válido"
+        entrar = false 
+        document.getElementById("exampleInputEmail1").style.border = "3px solid red"
+    }else{
+      document.getElementById("exampleInputEmail1").style.border = "none"
+
+    }
+ 
+
+    if(inputContra.value.length<6){//para la primera contraseña
+        document.getElementById("errorContra").innerHTML = `La contraseña es muy corta <br>`
+        entrar = false
+        document.getElementById("exampleInputPassword").style.border = "3px solid red"
+    }else{
+      document.getElementById("exampleInputPassword").style.border = "none"
     }
 
-    if (entrar) {
-      parrafowarnings.innerHTML = warnings;
-    } else {
-      parrafowarnings.innerHTML = "Formulario enviado correctamente";
+
+    if(!contraRegExp.test(inputContra.value)){//para que pida un caracter especial 
+        document.getElementById("errorContra1").innerHTML = "Introduce al menos un caracter especial"
+        entrar = false
+        
+    }
+
+
+    if(inputContraRep.value!=inputContra.value){//confirmacion de la contraseña 
+        document.getElementById("errorContraRep").innerHTML = "Las contraseñas no coinciden"
+        entrar = false
+        document.getElementById("exampleInputPassword2").style.border = "3px solid red"
+
+    }else{
+      document.getElementById("exampleInputPassword2").style.border = "none"
+
+
+    }
+
+
+    if(inputEdad.value <18 || inputEdad.value >64){
+      document.getElementById("errorEdad").innerHTML =`<br>La edad debe ser de entre 18 y 64 años`
+      entrar = false
+      document.getElementById("edad").style.border = "3px solid red"
+    }else{
+      document.getElementById("edad").style.border = "none"
     }
 
 
 
+    if (entrar) {//habilitar boton 
+      document.getElementById("boton-registro").disabled = false//quita el disable que puse en html
+      
+    }else{
+      
+      document.getElementById("boton-registro").disabled = true//continua desabilitado(por si lleno todos los campos y luego borro alguno)
+      confirmacionEntrada.innerHTML = ""
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 
@@ -70,18 +114,22 @@ document.addEventListener("DOMContentLoaded", () => {//esperar a que cargue el D
   });//cierre addevent
 
 
+  form.addEventListener("submit", e =>{//se permitira el evento submit solo cuando todos los campos sean correctos 
+     e.preventDefault();//evita que se recargue la pagina
+     let entrar = true;
 
 
+    if (entrar) {//si hay erroes los muestra 
+      confirmacionEntrada.style.color = "rgb(17, 255, 0)";//mod estilos desde js 
+      confirmacionEntrada.innerHTML = "Formulario enviado de manera correcta"
+
+      
+    }else{
+      confirmacionEntrada.innerHTML = ""
+    }
 
 
-
-
-
-
-
-
-
-
+  })//cierre del submit 
 
 
 
