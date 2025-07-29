980a6dd0e22ff770d95104030dc73a7f61776b62
David19 <davidleonel911@gmail.com>
2025-07-29 14:57:08 -0500

    enviar mas datos(listas)

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 
 #uso de mas variables y datos 
 
+#no solo podemos enviar variables sino otros tipos de datos
+#vamos a hacer el ejemplo enviando listas 
 from flask import Flask, render_template
 
 app = Flask(__name__)#
@@ -10,12 +12,21 @@ app = Flask(__name__)#
 def index():
 
     name = "David"
+    numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]#lista que vamos a imprimir 
 
-    return render_template("index.html", name = name)#
+    return render_template("index.html", name = name, numeros = numeros)#
 
 
+#en el html
+"""
+    <ul>
+        <!--asi imprimimos una lista desde python-->
+        {%for numero in numeros%}
+            <li> {{ numero }} </li> 
+        {% endfor %}
 
-
+    </ul>
+"""
 
 
 
